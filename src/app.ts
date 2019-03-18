e9339226816d9dc1ada3c5d6e0da8fb460b93ccd
aelbore <aelbore.sg@gmail.com>
2019-03-18 23:01:10 +0800

    update to ng-8-beta.8

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,7 @@ import { Component, ɵrenderComponent as renderComponent } from '@angular/core';
   template: `<h1>Hello World. {{ name }}</h1>`
 })
 export class HelloWorldComponent {
-
   name = "Arjay";
-
 }
 
-const host = document.createElement('hello-world');
-document.body.appendChild(host);
-
-renderComponent(HelloWorldComponent, { host });
+renderComponent(HelloWorldComponent);
